979d7b9c599c0a67c59787948b84931067a5f7ef
Jason Vaux <jasonavaux@gmail.com>
2026-04-19 15:54:38 -0400

    Modify app.run to use environment PORT variable
    Update app to run on a specified port from environment variable

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+import os
+
+
 
 app = Flask(__name__)
 CORS(app)
@@ -55,4 +58,5 @@ def gps():
     })
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    PORT = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=PORT)
